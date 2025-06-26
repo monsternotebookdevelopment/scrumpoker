98e5a7ab20198be1fe50bb242b293e3e6bf98378
Sercan Solmaz <sercan.solmaz@monsternotebook.com>
2025-06-26 17:35:18 +0300

    Add base.

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,18 @@ import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/scrum-poker/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      '/socket.io': {
+        target: 'http://localhost:3000', // Node.js sunucunuzun adresi
+        ws: true, // WebSocket bağlantıları için gereklidir
+      },
+    }
+  }
 });
