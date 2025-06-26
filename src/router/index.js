27c9c5e89b79b221ec77e68c752ca7b60b50becd
Sercan Solmaz <sercan.solmaz@monsternotebook.com>
2025-06-26 18:20:13 +0300

    Update router.

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/scrum-poker/'), 
   routes: [
     {
       path: "/",
@@ -15,6 +15,5 @@ const router = createRouter({
     },
   ],
 });
-
 
 export default router;
